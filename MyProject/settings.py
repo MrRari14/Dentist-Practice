b35bb0e6a268d88bef39b54c2b9ca1beb6f3c658
Bernardo <bernardoferraricarp@gmail.com>
2020-12-29 12:22:50 +0100

    app for heroku

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -17,6 +17,7 @@ from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -123,7 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
